1c9d8782ba2cb6e2bb1c149e35e2bbf7d0681af9
cosmosjoen <cosmosjeon1108@gmail.com>
2025-07-03 03:20:30 +0900

    JavaScript - handling events

--- 2주차 쇼핑몰 미니게임/main.js
+++ 2주차 쇼핑몰 미니게임/main.js
@@ -1,14 +1,14 @@
 // Fetch the items from the JSON file
 function loadItems() {
-  return fetch("data/data.json")
-    .then((response) => response.json())
-    .then((json) => json.items);
+  return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
 }
 
 // Update the list with the given items
 function displayItems(items) {
-  const container = document.querySelector(".items");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
 // Create HTML list item from the given data item
@@ -21,10 +21,29 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
-  .then((items) => {
+  .then(items => {
     displayItems(items);
-    // setEventListeners(items);
+    setEventListeners(items);
   })
   .catch(console.log);
